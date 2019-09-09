bca15922f5e8875663f88b206cd118ca455d535e
gadfrey <gadfreybalacy@gmail.com>
2019-09-08 19:55:15 -0700

    Updates

---
+++ precache-manifest.e439c64c2fe50d1f0f774137e82fdfac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7888d7e21d3146ca158726f11c8275",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "fc2fc74d5d7cdb105524",
+    "url": "/starwars/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4c22332f5cfd81d60503",
+    "url": "/starwars/static/css/main.77e864d6.chunk.css"
+  },
+  {
+    "revision": "fc2fc74d5d7cdb105524",
+    "url": "/starwars/static/js/2.34658862.chunk.js"
+  },
+  {
+    "revision": "4c22332f5cfd81d60503",
+    "url": "/starwars/static/js/main.139b44af.chunk.js"
+  },
+  {
+    "revision": "b8accd5fd9af79b65845",
+    "url": "/starwars/static/js/runtime~main.864a284e.js"
+  }
+]);
